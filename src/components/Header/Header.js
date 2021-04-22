2d369d2bbdc0ea3621a4a3b28dccc70abdc387ad
mhdnaldi <mhd.naldi@yahoo.com>
2021-04-23 03:16:35 +0700

    add responsive styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import EventNoteIcon from "@material-ui/icons/EventNote";
 
 import { NavLink } from "react-router-dom";
 
 const Header = (props) => {
+  const [show, setShow] = useState(false);
   return (
     <div className='header'>
-      <div className='header__logo'>LOGO</div>
-      <div className='header__link'>
+      <div className='header__logo'>
+        <div className='logo'>
+          <EventNoteIcon
+            fontSize='large'
+            style={{
+              color: "#2ac4f3",
+            }}
+          />
+        </div>
+        <div className='burger__menu'>
+          <MenuIcon
+            fontSize='large'
+            onClick={() => setShow((prevState) => !prevState)}
+          />
+        </div>
+      </div>
+      <div className={`${show ? "header__link show" : "header__link none"}`}>
         <NavLink exact activeStyle={{ color: "#111" }} to='/'>
-          EVENTS
+          HOME
         </NavLink>
         <NavLink exact activeStyle={{ color: "#111" }} to='/add-event'>
           ADD EVENT
@@ -18,6 +36,11 @@ const Header = (props) => {
           DASHBOARD
         </NavLink>
       </div>
+      {show && (
+        <div
+          className='backdrop'
+          onClick={() => setShow((prevState) => !prevState)}></div>
+      )}
     </div>
   );
 };
